74a58b4ca23359ca59b7934bda008534f3090801
Ayush190730 <148128641+Ayush190730@users.noreply.github.com>
2025-01-09 11:34:22 +0530

    server.js

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ const app = express();
 
 const PORT = process.env.PORT || 2000;
 
+// Cors 
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+  }
+  
+  // Default configuration looks like
+  // {
+  //     "origin": "*",
+  //     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  //     "preflightContinue": false,
+  //     "optionsSuccessStatus": 204
+  //   }
+  
+  app.use(cors(corsOptions));
+
 //middlewares
 app.use(express.static('Public'));
 app.use(express.json());
